7eec986cf88b0083d759f2469ec0b8ed5a20df1a
Juan Felipe Gonzalez Ortiz <53414652+felipegonzalezmv@users.noreply.github.com>
2026-09-15 03:27:08 -0500

    Fix code quality AI findings reported by code scanning tools (#191)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,7 @@
 
 ### Fixed
 - SLF4J "no provider" warnings during startup [(#1577)](https://github.com/wazuh/wazuh-indexer/issues/1577)
+- Ship a secure-by-default `host_deny_list` for notification egress, blocking SSRF to loopback, private and cloud-metadata addresses [(#1853)](https://github.com/wazuh/wazuh-indexer/issues/1853)
 
 ## Prior versions
 - []()
--- notifications/notifications/src/main/kotlin/org/opensearch/notifications/index/ConfigIndexingActions.kt
+++ notifications/notifications/src/main/kotlin/org/opensearch/notifications/index/ConfigIndexingActions.kt
@@ -436,8 +436,13 @@ object ConfigIndexingActions {
             }
         }
         val configSearchResult = configDocs.map {
+            val configId = it.docInfo.id
+                ?: throw OpenSearchStatusException(
+                    "NotificationConfig document is missing its document id",
+                    RestStatus.INTERNAL_SERVER_ERROR
+                )
             NotificationConfigInfo(
-                it.docInfo.id!!,
+                configId,
                 it.configDoc.metadata.lastUpdateTime,
                 it.configDoc.metadata.createdTime,
                 it.configDoc.config
@@ -484,8 +489,8 @@ object ConfigIndexingActions {
             val config = it.notificationConfig
             Channel(configId, config.name, config.description, config.configType, config.isEnabled)
         }
-        val ChannelList = ChannelList(searchResult)
-        return GetChannelListResponse(ChannelList)
+        val channelList = ChannelList(searchResult)
+        return GetChannelListResponse(channelList)
     }
 
     private fun getSupportedChannelList(): List<String> {
